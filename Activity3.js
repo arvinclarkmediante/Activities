f92e22be48a88d0bd4b8eafa399e050501a3f9db
Louie Jay <arvinclarkmediante@gmail.com>
2026-09-21 21:31:22 +0800

    Const Variables

---
+++ Activity3.js
@@ -0,0 +1,11 @@
+// 10 CONST VARIABLES
+const school = "NWSSU";
+const passScore = 70;
+const maxAge = 18;
+const term = "Fall";
+const fee = 1000;
+const student1 = { name: "Arvin Clark Mediante ", score: 96, contact: { email: "arvinclarkmediante@gmail.com" } };
+const student2 = { name: "Jayrold kim", score: 95, contact: { phone: "09924536718" } };
+const student3 = { name: "Arnold", score: 80, contact: { phone: "00446278190" } }; 
+const listA = ["Math", "english"];
+const listB = [80, 90];
