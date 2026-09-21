19aa1a6946c6178e58439615b8c4fd2af482188f
Louie Jay <arvinclarkmediante@gmail.com>
2026-09-21 21:32:10 +0800

    Let Variables

--- Activity3.js
+++ Activity3.js
@@ -9,3 +9,15 @@ const student2 = { name: "Jayrold kim", score: 95, contact: { phone: "0992453671
 const student3 = { name: "Arnold", score: 80, contact: { phone: "00446278190" } }; 
 const listA = ["Math", "english"];
 const listB = [80, 90];
+
+// 10 LET VARIABLES
+let room = "Class 3-A";
+let totalStudents = 3;
+let status = "Active";
+let leader = "Arvin Clark Mediante";
+let topScore = 96;
+let lowScore = 80;
+let avgScore = 90.3;
+let isOpen = true;
+let totalClubs = 5;
+let activeUser = "Jayrold kim";
