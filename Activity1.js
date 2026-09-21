413bf58071b067ec274d5c3c1f1a3b15e84165d7
Louie Jay <arvinclarkmediante@gmail.com>
2026-09-21 20:44:15 +0800

    Conditions

--- Activity1.js
+++ Activity1.js
@@ -7,3 +7,20 @@ let passing = 15;
 let quiz = [5, 4, 3];
 let bonus = [1, 1, 1];
 let subjects = ["Math", "Science", "History"];
+
+// CONDITIONALS
+if (quiz[0] >= 5) {
+    console.log("Math Passed");
+} else {
+    console.log("Math Failed");
+}
+if (quiz[1] >= 4) {
+    console.log("Science Passed");
+} else {
+    console.log("Science Failed");
+}
+if (quiz[2] >= 3) {
+    console.log("History Passed");
+} else {
+    console.log("History Failed");
+}
